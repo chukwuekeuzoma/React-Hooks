9fbe3810c88a9f754cc3bdc1941bc9e9fcdb9692
mac <chukwuekeuzoma@gmail.com>
2022-11-07 21:01:31 +0100

    updated the readme

--- src/Hooks/UseStateTutor.jsx
+++ src/Hooks/UseStateTutor.jsx
@@ -4,12 +4,9 @@ function UseStateTutor(props) {
    const [counter, setCounter] = useState(0)
 
    const increment = () => {
-    //  setCounter((prev)=> prev + 1 )
-     
+    //  setCounter((prev)=> prev + 1 ) 
     //  or you can do it like this
-
     setCounter(counter + 1)
-
    }
 
     return (
