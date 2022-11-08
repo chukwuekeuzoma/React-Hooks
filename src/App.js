9c55356febb6f9bd81ec4063bf7ed0c769e588ab
mac <chukwuekeuzoma@gmail.com>
2022-11-08 23:53:40 +0100

    updated the component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import {UseStateTutor, UseReducerTutor} from "./Hooks"
+import {UseStateTutor, UseReducerTu, UseRefTutor } from "./Hooks"
 
 function App() {
   return (
     <div style={{ display:"flex", justifyContent:"center", alignItems: "center", marginTop:"10%"}}>
-      {/* <UseStateTutor/> */}
-      <UseReducerTutor/>
+      {/* <UseStateTutor/>
+      <UseReducerTutor/> */}
+      <UseRefTutor />
     </div>
   );
 }
