e35f73bd4e7059401455c22c02c3a6bbdb0c400a
mac <chukwuekeuzoma@gmail.com>
2022-11-10 01:11:21 +0100

    updated the component

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -1,11 +1,13 @@
-import UseStateTutor from "./UseStateTutor"
-import UseReducerTutor from "./UseReducerTutor"
-import UseRefTutor from "./UseRefTutor"
-import ImperitiveHandleTutor from "./ImperitiveHandleTutor"
+import UseStateTutor from "./UseStateTutor";
+import UseReducerTutor from "./UseReducerTutor";
+import UseRefTutor from "./UseRefTutor";
+import ImperitiveHandleTutor from "./ImperitiveHandleTutor";
+import ReactMemoTutor from "./ReactMemoTutor";
 
 export {
-    UseStateTutor,
-    UseReducerTutor,
-    UseRefTutor,
-    ImperitiveHandleTutor
-}
+  UseStateTutor,
+  UseReducerTutor,
+  UseRefTutor,
+  ImperitiveHandleTutor,
+  ReactMemoTutor,
+};
