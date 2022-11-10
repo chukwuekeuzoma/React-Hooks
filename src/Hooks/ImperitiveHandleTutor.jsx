ecfe94060f48a0e35bd78721d1cd233c7d2045a4
mac <chukwuekeuzoma@gmail.com>
2022-11-10 01:11:05 +0100

    updated the component

--- src/Hooks/ImperitiveHandleTutor.jsx
+++ src/Hooks/ImperitiveHandleTutor.jsx
@@ -1,23 +1,18 @@
-import React,{ useRef } from 'react';
-import Button from '../Components/Button'
-
+import React, { useRef } from "react";
+import Button from "../Components/Button";
 
 function ImperitiveHandleTutor(props) {
-    const buttonRef = useRef(null)
-    
-    const onClick =  () => {
-       buttonRef.current.alterToogle()
-    }
-    return (
-        <div>
-             <button
-               onClick={onClick}
-             >
-                Button from Parent
-             </button>
-             <Button  ref={buttonRef}/>
-        </div>
-    );
+  const buttonRef = useRef(null);
+
+  const onClick = () => {
+    buttonRef.current.alterToogle();
+  };
+  return (
+    <div>
+      <button onClick={onClick}>Button from Parent</button>
+      <Button ref={buttonRef} />
+    </div>
+  );
 }
 
 export default ImperitiveHandleTutor;
