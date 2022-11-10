3f37e141c44b7f893df1b7eb2595abe29a31edce
mac <chukwuekeuzoma@gmail.com>
2022-11-10 01:10:59 +0100

    updated the component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,23 +1,21 @@
-import React,{useImperativeHandle, forwardRef, useState} from 'react';
+import React, { useImperativeHandle, forwardRef, useState } from "react";
 
 const Button = forwardRef((props, ref) => {
-    const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
-    useImperativeHandle(ref, ()=> ({
-        alterToogle() {
-          setToggle(!toggle)
-       }
-    }))
+  useImperativeHandle(ref, () => ({
+    alterToogle() {
+      setToggle(!toggle);
+    },
+  }));
 
-    return (
-        <div>
-             <button>
-               Button from Child
-             </button>
-             <br/>
-             {toggle && <span>Toogle</span>}
-        </div>
-    );
+  return (
+    <div>
+      <button>Button from Child</button>
+      <br />
+      {toggle && <span>Toogle</span>}
+    </div>
+  );
 });
 
 export default Button;
